57ed990c1ebdc7784bf62ebbfd2188853ded0450
stoppini <rove.albe@gmail.com>
2023-09-13 12:09:20 +0200

    First up-mabfws implementation

--- setup.py
+++ setup.py
@@ -1,60 +1,40 @@
 #!/usr/bin/env python3
 import subprocess
-
-from setuptools import setup  # type: ignore
-from setuptools.command.build_py import build_py  # type: ignore
-from setuptools.command.develop import develop  # type: ignore
-import os
-import urllib
-import shutil
-
-
-MA_BFWS_dst = "./ma_bfws/MA_BFWS"
-MA_BFWS_PUBLIC = "ma_bfws"
-# COMPILE_CMD = './compile'
-MA_BFWS_TAG = "master"
-MA_BFWS_REPO = "https://.../MA_BFWS"
+import platform
+from setuptools import setup, Distribution
 
 long_description = """============================================================
     UP_MA_BFWS
  ============================================================
 """
-isExist = os.path.exists("up_ma_bfws")
-if not isExist:
-    os.system("mkdir up_ma_bfws")
 
+arch = (platform.system(), platform.machine())
 
-def install_MA_BFWS():
-    subprocess.run(["git", "clone", "-b", MA_BFWS_TAG, MA_BFWS_REPO])
-    shutil.move(MA_BFWS_PUBLIC, MA_BFWS_dst)
-    curr_dir = os.getcwd()
-    os.chdir(MA_BFWS_dst)
-    # subprocess.run(COMPILE_CMD)
-    os.system("rm -r out 2> /dev/null")
-    os.system("rm -r ma_bfws-dist 2> /dev/null")
-    os.system("mkdir ma_bfws-dist")
-    #Examples:
-    # os.system('cp -r libs/ enhsp-dist/')
-    # os.system("cp FMAP.jar fmap-dist/")
-    # Add MA_BFWS
-    os.chdir(curr_dir)
+EXECUTABLES = {
+    ("Linux", "x86_64"): "maBFWS",
+    # ("Windows", "x86_64"): "maBFWS",
+    # ("Windows", "AMD64"): "maBFWS",
+}
 
+executable = EXECUTABLES[arch]
 
-class InstallMA_BFWS(build_py):
-    """Custom install command."""
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
 
-    def run(self):
-        install_MA_BFWS()
-        build_py.run(self)
+except ImportError:
+    bdist_wheel = None
 
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(self):
+        return True
 
-class InstallMA_BFWSdevelop(develop):
-    """Custom install command."""
-
-    def run(self):
-        install_MA_BFWS()
-        develop.run(self)
-
+    def is_pure(self):
+        return False
 
 setup(
     name="up_ma_bfws",
@@ -63,7 +43,9 @@ setup(
     author="Alfonso E. Gerevini, Nir Lipovetzky, Francesco Percassi, Alessandro Saetti and Ivan Serina",
     author_email="ivan.serina@unibs.it",
     packages=["up_ma_bfws"],
-    package_data={"": ["MA_BFWS/..."]},
-    cmdclass={"build_py": InstallMA_BFWS, "develop": InstallMA_BFWSdevelop},
+    package_data={"up_ma_bfws": [executable]},
+    distclass=BinaryDistribution,
+    include_package_data=True,
+    cmdclass={'bdist_wheel': bdist_wheel},
     license="APACHE",
 )
